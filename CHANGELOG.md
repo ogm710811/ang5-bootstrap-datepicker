f02415e06394105653eabf50b072c6789279d1f8
ogarci <ogarcia@sapient.com>
2018-03-19 23:16:56 -0400

    adding folders and files

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+<a name="1.0.0-beta"></a>
+
+### Bug Fixes
+
+### Features
+
+
+
+
---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export { listLocales } from './chronos/locale/locales';
+export { setTheme } from './utils/theme-provider';
+export { DateFormatter, DatePickerComponent, DatepickerConfig, DatepickerModule, DayPickerComponent, MonthPickerComponent, YearPickerComponent, BsDatepickerModule, BsDatepickerConfig, BsDaterangepickerConfig, BsLocaleService, BsDaterangepickerDirective, BsDatepickerDirective } from './datepicker/index';
+export { OnChange, LinkedList, isBs3, Trigger, Utils } from './utils/index';
+export { ComponentLoader, ComponentLoaderFactory, ContentRef } from './component-loader/index';
+export { Positioning, PositioningOptions, PositioningService, positionElements } from './positioning/index';
+export { defineLocale, getSetGlobalLocale, LocaleData } from './chronos/index';
+export * from './locale';
